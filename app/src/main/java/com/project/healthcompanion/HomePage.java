43938a04a1ad4d1bed71bdb6eb90b2dbadb12e49
JoshNor73 <joshuanoronha73@gmail.com>
2021-05-26 09:01:40 +0400

    I've added a navigation drawer and the beginnings of a reminders page

---
+++ app/src/main/java/com/project/healthcompanion/HomePage.java
@@ -0,0 +1,116 @@
+package com.project.healthcompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomePage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        drawerLayout = findViewById(R.id.drawer_Layout);
+    }
+
+    public void ClickMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+
+    public void ClickLogo(View view) {
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickProfile(View view) {
+        //redirectActivity(this, Profile.class);
+    }
+
+    public void ClickHome(View view) {
+        closeDrawer(drawerLayout);
+    }
+
+
+    public void ClickDashboard(View view) {
+        //redirectActivity(this, Dashboard.class);
+    }
+
+    public void ClickGraphs(View view) {
+        //redirectActivity(this, Graphs.class);
+    }
+
+    public void ClickDietPlans(View view) {
+        //redirectActivity(this, DietPlans.class);
+    }
+
+
+    public void ClickRecipes(View view) {
+        //redirectActivity(this, Recipes.class);
+    }
+
+    public void ClickReminders(View view) {
+        redirectActivity(this, Reminders.class);
+    }
+
+    public void ClickSocial(View view) {
+        //redirectActivity(this, Social.class);
+    }
+
+
+    public void ClickLogout(View view) {
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to log out?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dismiss dialogue
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}
--- app/src/main/java/com/project/healthcompanion/MainActivity.java
+++ app/src/main/java/com/project/healthcompanion/MainActivity.java
@@ -2,7 +2,11 @@ package com.project.healthcompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Start(View view) {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
     }
 }
